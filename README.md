3e3bbd5695e08f90217b4019f3f56b0509cffcdb
Roman Kolivashko <rkolivashko@gmail.com>
2021-11-10 09:27:23 -0800

    Updated readme

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 # Power Plant
 
-#### (Brief Description of Application)
+#### Simple application for plant gardening by use of functional programming 
 
-#### By (Your Name Here)
+#### By Roman Kolivashko
 
 ## Technologies Used
 
-* _List all_
-* _the major technologies_
-* _you used in your project_
-* _here_
+* _JavaScript_
+* _JQuery_
+* _Webpack_
+* _Jest_
 
 ## Description
 
@@ -17,26 +17,25 @@
 
 1. Click on "Use This Template" button at top of GitHub repository. 
 2. Name your repository (ex. my-project)
-3. Clone the repository: $ git clone https://github.com/account/my-project
-4. Navigate to the my-project/ directory on your computer
+3. Clone the repository: $ git clone https://github.com/romankolivashko/powerPlant.git
+4. Navigate to the powerPlant/ directory on your computer
 5. Open with your preferred text editor to view the code base
 6. Update code to match name of project:
-   * webpac.config.js - line 19
+   * webpack.config.js - line 19
    * package.json - line 2
    * package-lock.json - line 2
    * index.html - line 4
 7. To start a development server and view the project in the browser:
-   * Navigate to my-project/ in your command line
+   * Navigate to powerPlant/ in your command line
    * Run the command `npm install` to install dependencies
    * Optionally, run the commmand `npm run build` to make a bundle of the files
    * Finally, run the command `npm run start` to start a development server
  
-To run tests, navigate to my-project/ in your terminal and run the command `npm run test`
+To run tests, navigate to powerPlant/ in your terminal and run the command `npm run test`
 
 ## Known Bugs
 
-* _Any known issues_
-* _should go here_
+* [Issues](https://github.com/romankolivashko/powerPlant/issues)
 
 ## License
 
--- src/index.js
+++ src/index.js
@@ -1,27 +1,75 @@
-import $ from 'jquery';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/styles.css';
-import Triangle from './js/triangle.js';
-import Rectangle from './js/rectangle.js';
+// This function stores our state.
+
+const storeState = () => {
+  let currentState = {};
+  return (stateChangeFunction = state => state) => {
+    const newState = stateChangeFunction(currentState);
+    currentState = {...newState};
+    return newState;
+  }
+}
+
+const stateControl = storeState();
+
+// This is a function factory. We can easily create more specific functions that alter a plant's soil, water, and light to varying degrees.
+
+const changeState = (prop) => {
+  return (value) => {
+    return (state) => ({
+      ...state,
+      [prop] : (state[prop] || 0) + value
+    })
+  }
+}
+
+// We create four functions using our function factory. We could easily create many more.
+
+const feed = changeState("soil")(1);
+const blueFood = changeState("soil")(5);
+
+const hydrate = changeState("water")(1);
+const superWater = changeState("water")(5);
+
+const exposeToSun = changeState("sun")(1);
+const sunshine = changeState("sun")(5);
 
 $(document).ready(function() {
-  $('#triangle-checker-form').submit(function(event) {
-    event.preventDefault();
-    const length1 = parseInt($('#length1').val());
-    const length2 = parseInt($('#length2').val());
-    const length3 = parseInt($('#length3').val());
-    const triangle = new Triangle(length1, length2, length3);
-    const response = triangle.checkType();
-    $('#response').append(`<p>${response}</p>`);
+
+// This function has side effects because we are using jQuery. Manipulating the DOM will always be a side effect. Note that we only use one of our functions to alter soil. You can easily add more.
+
+  $('#feed').click(function() {
+    const newState = stateControl(blueFood);
+    $('#soil-value').text(`Soil: ${newState.soil}`);
   });
 
-  $('#rectangle-area-form').submit(function(event) {
-    event.preventDefault();
-    const length1 = parseInt($('#rect-length1').val());
-    const length2 = parseInt($('#rect-length2').val());
-    const rectangle = new Rectangle(length1, length2);
-    const response = rectangle.getArea();
-    $('#response2').append(`<p> The area of the rectangle is ${response}.</p>`);
+  $('#hydrate').click(function() {
+    const newState = stateControl(superWater);
+    $('#water-value').text(`Water: ${newState.water}`);
+  });
+
+  $('#sun').click(function() {
+    const newState = stateControl(sunshine);
+    $('#sun-value').text(`Sun: ${newState.sun}`);
+  });
+
+// This function doesn't actually do anything useful in this application - it just demonstrates how we can "look" at the current state (which the DOM is holding anyway). However, students often do need the ability to see the current state without changing it so it's included here for reference.
+
+  $('#show-state').click(function() {
+    // We just need to call stateControl() without arguments to see our current state.
+    const currentState = stateControl();
+    $('#soil-value').text(`Soil: ${currentState.soil}`);
   });
 });
+
+let removeDuplicates= function(nums){
+    if(!nums.length) return 0;
+
+    let i = 0;
+    for (let j = 1; j < nums.length; j++){
+      if (nums[i] !== num[j]){
+        i++;
+        nums[i] = nums[j];
+      }
+    }
+    return i + 1;
+}
